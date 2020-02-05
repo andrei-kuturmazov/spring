e2d2c368e73d4ccd156ccec0f9d114fbf9583376
andrei-kuturmazov <andrei.kuturmazov@gmail.com>
2020-02-05 11:16:40 +0300

    Second bean adding

--- src/com/springDemo/HelloSpringApp.java
+++ src/com/springDemo/HelloSpringApp.java
@@ -4,9 +4,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloSpringApp {
     public static void main(String[] args) {
+
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Coach firstSpringCoach = context.getBean("myCoach", Coach.class);
+        Coach secondSpringCoach = context.getBean("secondCoach", Coach.class);
         System.out.println(firstSpringCoach.getDailyWorkout());
+        System.out.println(secondSpringCoach.getDailyWorkout());
         context.close();
+
     }
 }
